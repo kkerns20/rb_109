58ff3f48d1b60202f4f7472255123393fe2a1ddf
Kurt Kerns <84055816+kkerns20@users.noreply.github.com>
2022-02-28 09:18:53 -0600

    solution for dettect pangram

---
+++ interview_assessment/practice_problems/detect_pangram.rb
@@ -0,0 +1,39 @@
+=begin
+Problem
+------------------------------------------
+Given a string, detect whether it is a pangram. Return true if it is, false if not. Ignore numbers and punctuation. 
+
+Inputs: 1 string
+Outputs: Boolean
+
+Rules/Requirements
+- A pangram is a sentence that contains every single letter of hte alphabet at least once.
+
+Clarifying Questions
+- 
+
+Examples, Test Cases
+------------------------------------------
+p panagram?("The quick brown fox jumps over the lazy dog.") == true
+Every letter in the alphabet is used within the given string
+Therefore, this will return 'true'
+
+Data Structure, Algorithm
+------------------------------------------
+==> Breakdown string into characters and then check and see if every letter of the alphbet has been used.
+
+-- method --> pangram?(string) --> boolean
+  - create an array of all of the letters in the alphabet
+  -determine if all letters of the alphabet are found in the array of alpha letters
+    - return true if so
+    - otherwise, false
+=end
+
+def pangram?(string)
+  ('a'..'z').to_a.all? do |letter|
+    string.downcase.include?(letter)
+  end
+end
+
+p pangram?("The quick brown fox jumps over the lazy dog.") == true
+p pangram?("This is not a pangram.") == false
