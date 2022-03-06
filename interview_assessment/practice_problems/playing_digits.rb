b44ff8c24b4eb5e8974374b63e7bfce439bc80dd
Kurt Kerns <84055816+kkerns20@users.noreply.github.com>
2022-03-06 14:10:28 -0600

    updated solution

--- interview_assessment/practice_problems/playing_digits.rb
+++ interview_assessment/practice_problems/playing_digits.rb
@@ -40,6 +40,10 @@ def dig_pow(n, p)
   remainder == 0 ? k : -1
 end
 
+def dig_pow(n, p)
+    total = n.to_s.split('').map.with_index{|d, i| d.to_i ** (p+i)}.reduce(:+)
+    total % n == 0 ? (total / n) : -1
+end
 
 p dig_pow(89, 1) == 1
 p dig_pow(92, 1) == -1
