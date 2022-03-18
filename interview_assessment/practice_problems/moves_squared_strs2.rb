b98ffc947a24815fd26968ad18923a11c803acef
Kurt Kerns <84055816+kkerns20@users.noreply.github.com>
2022-03-18 14:19:04 -0500

    updated solution

--- interview_assessment/practice_problems/moves_squared_strs2.rb
+++ interview_assessment/practice_problems/moves_squared_strs2.rb
@@ -1,1 +1,30 @@
+def vert_mirror(strng)
+  strng.split("\n").map(&:reverse).join("\n")
+end
 
+def hor_mirror(strng)
+  strng.split("\n").reverse.join("\n")
+end
+
+def oper(fct, s) 
+  fct.call(s)
+end
+
+def rot(s)
+  s = vert_mirror(s)
+  hor_mirror(s)
+end
+
+def selfie_and_rot(s)
+  arr = s.split("\n").map do |x|
+    "#{x}#{x.reverse}"
+  end
+  (arr + arr.reverse).join("\n")
+end
+
+
+
+s = "abcd\nefgh\nijkl\nmnop"
+
+p rot(s)
+p selfie_and_rot(s)
