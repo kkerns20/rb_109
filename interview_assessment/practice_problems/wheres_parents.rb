9b0449d50443bbba4271d25773724bd008fac0ac
Kurt Kerns <84055816+kkerns20@users.noreply.github.com>
2022-03-23 11:46:47 -0500

    updated-refactored and redone solution

--- interview_assessment/practice_problems/wheres_parents.rb
+++ interview_assessment/practice_problems/wheres_parents.rb
@@ -1,3 +1,12 @@
 def find_children(string)
   string.chars.sort_by { |char| [char.downcase, char] }.join
 end
+
+def find_child(str)
+  ('A'..'Z').to_a.map.with_object([]) do |char, arr|
+    if str.include?(char)
+      arr << char << char.downcase *
+        str.count(char.downcase)
+    end
+  end.join
+end
