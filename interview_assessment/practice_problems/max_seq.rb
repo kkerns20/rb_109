4ad46413bf23651a7f3280a7e8201521ebacc132
Kurt Kerns <84055816+kkerns20@users.noreply.github.com>
2022-03-24 13:22:38 -0500

    faster second solution

--- interview_assessment/practice_problems/max_seq.rb
+++ interview_assessment/practice_problems/max_seq.rb
@@ -52,3 +52,59 @@ p max_sequence([]) == 0
 p max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6
 p max_sequence([11]) == 11
 p max_sequence([-32]) == 0
+
+=begin
+SECOND ATTEMPT - 8 or so minutes at max
+Problem
+------------------------------------------
+Find the maximum sum of a subarray
+
+Inputs: 1 array 
+Outputs: 1 integer
+
+Rules/Requirements
+- 
+
+Clarifying Questions
+- 
+
+Examples, Test Cases
+------------------------------------------
+
+
+Data Structure, Algorithm
+------------------------------------------
+return 0 if all are < 0
+sub_arr = the return of finding all of the subarrs with find_subs
+iterate through sub_arr with transformation and sum then find the max
+
+-- find sub
+set subs = empty arr
+from 1 upto arr size do l
+  arr.each_cons to l do subarr
+    push subarr to subs
+return subs
+=end
+
+def find_subs(arr)
+  subs = []
+  1.upto(arr.size) do |l|
+    arr.each_cons(l) do |sub_arr|
+      subs << sub_arr
+    end
+  end
+  subs
+end
+
+def max_seq(arr)
+  return 0 if arr.all? { |x| x < 0 }
+  find_subs(arr).map(&:sum).max
+end
+
+# p find_subs([1,2,3,4,4,5])
+
+p max_seq([]) == 0
+p max_seq([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6
+p max_seq([11]) == 11
+p max_seq([-32]) == 0
+p max_seq([-2, 1, -7, 4, -10, 2, 1, 5, 4]) == 12
